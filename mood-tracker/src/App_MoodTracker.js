6c13d1d67a72943adeb3fa6d9743e41de8b1cfe2
Hang Nguyen <52890478+hangnguyen81@users.noreply.github.com>
2021-09-14 22:33:55 +0300

    upadate error message

--- mood-tracker/src/App_MoodTracker.js
+++ mood-tracker/src/App_MoodTracker.js
@@ -2,6 +2,7 @@ import { useState } from "react"
 
 const App_MoodTracker = () =>{
     const [message, setMessage] = useState('')
+    const [error, setError] = useState(false)
     const defaultMoodsArray = ['Wonderful', 'Good', 'Normal', 'Bad', 'Terrible']
     const maxlength = 20
     const defaultMoods = defaultMoodsArray.map( mood =>{
@@ -9,20 +10,33 @@ const App_MoodTracker = () =>{
                 <button 
                     key={mood} 
                     value={mood}
-                    onClick = {(e) => setMessage(e.target.value)}
+                    onClick = {(e) => {
+                        setMessage(e.target.value)
+                        setError(false)
+                        }
+                    }
                 >
                     {mood}
                 </button>
             )
     })
+
+
     return (
         <div className='mood-tracker'>
             <h1>How do you feel today?</h1>
             {defaultMoods}
-            <textarea className={message.length > maxlength? 'toolong':''}
+            <textarea className={error?'toolong':''}
                 value = {message}
                 placeholder='How are you today?'
-                onChange={(e) => setMessage(e.target.value)}
+                onChange={(e) => {
+                    setMessage(e.target.value)
+                    if(message.length > maxlength)
+                        setError(true)
+                    else
+                        setError(false)
+                     }
+                }
             />
             <p>Message length: {message.length}/ {maxlength}</p>
         </div>
