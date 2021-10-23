178998cffc6409705ef04a744d9cfdf92ccf4b97
Hang Nguyen <52890478+hangnguyen81@users.noreply.github.com>
2021-10-23 23:29:48 +0300

    Add files via upload

---
+++ slider/src/personsData.js
@@ -0,0 +1,58 @@
+const persons = [
+    {
+      id: 1,
+      name: 'susan Smith',
+      title: 'web developer',
+      image:
+        'images/persons/pic_01.jpg',
+      quote:
+        "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    },
+    {
+      id: 2,
+      name: 'anna johnson',
+      title: 'web designer',
+      image:
+        'images/persons/pic_02.jpg',
+      quote:
+        'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+    },
+    {
+      id: 3,
+      name: 'peter jones',
+      title: 'intern',
+      image:
+        'images/persons/pic_03.jpg',
+      quote:
+        'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+    },
+    {
+      id: 4,
+      name: 'bill anderson',
+      title: 'the boss',
+      image:
+        'images/persons/pic_04.jpg',
+      quote:
+        'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+    },
+    {
+        id: 5,
+        name: 'alex max',
+        title: 'hr manager',
+        image:
+          'images/persons/pic_05.jpg',
+        quote:
+          'Twee shabby chic taiyaki flannel, enamel pin venmo vape four loko. Hexagon kale chips typewriter kitsch 8-bit organic plaid small batch keffiyeh ethical banh mi narwhal echo park cronut.',
+      },
+      {
+        id: 6,
+        name: 'jason bright',
+        title: 'senior software developer',
+        image:
+          'images/persons/pic_06.jpg',
+        quote:
+          'The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.',
+      }
+  ];
+  
+  export default persons;
